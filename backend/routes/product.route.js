64179efc24e2b348979664ddd49268c84c997e4e
Marijan <castvan.marian@gmail.com>
2025-04-25 12:45:28 +0200

    project completed

---
+++ backend/routes/product.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { deleteProduct, updateProduct, createProduct, getProducts } from "../controllers/product.controller.js";
+
+const router = express.Router();
+
+router.get("/", getProducts);
+
+router.post("/", createProduct);
+
+router.put("/:id", updateProduct);
+
+router.delete("/:id", deleteProduct);
+
+
+export default router;
